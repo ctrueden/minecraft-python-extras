911609e6f988fbca4dfdf0cf8268d963c46863e1
Curtis Rueden <ctrueden@wisc.edu>
2021-01-26 19:36:38 -0600

    Wrap most functions into a Perspective class
    This is important because the mcapi global functions normally only work
    on the first world of a server. If you have more than one world (e.g.:
    you are in the Nether, or you use the Multiverse plugins) then they
    won't work as expected. With the Perspective class, you can now do:
    
      x = pov('yourusername')
    
    And you'll have an object whose functions always operate on you and/or
    the world you happen to be in right now.

--- __init__.py
+++ __init__.py
@@ -1,186 +1,18 @@
+import math
 from mcapi import *
-
-################# CONTAINERS #################
-
-#class Region(object):
-#    def __init__(self):
-#        pass
-#    def pos():
-# Region is a collection of blocks
-# Region is iterable
-
-# Player is a 1-block Region aligned with a player's position.
-# Box is an WxHxD Region.
-# FloodFilled Region? With/without bounds?
-# Support for composite Regions? (and/or)
-# Other Regions are possible.
-
-################# GAME MODES #################
-
 from org.bukkit import GameMode
-
-def creative(player=None):
-    if player is None: player = player()
-    player.gameMode = GameMode.CREATIVE
-
-def survival(player=None):
-    if player is None: player = player()
-    player.gameMode = GameMode.SURVIVAL
-
-#################### TIME ####################
-
-# NB: 24000 ticks per cycle.
-
-def dawn():
-    time(22500)
-
-def morning():
-    time(0)
-
-def noon():
-    time(6000)
-
-def dusk():
-    time(12500)
-
-def night():
-    time(14000)
-
-################## WEATHER ###################
-
-def sun():
-    weather(False, False)
-
-def rain():
-    weather(True, False)
-
-def storm():
-    weather(True, True)
-
-################## VIOLENCE ##################
-
-def zap():
-    bolt(lookingat())
-
-def boom(power=3):
-    explosion(lookingat(), power)
-
-################## POSITION ##################
-
-def x(pl=None):
-    if pl is None: pl = player()
-    return int(round(pl.location.x))
-
-def y(pl=None):
-    if pl is None: pl = player()
-    return int(round(pl.location.y))
-
-def z(pl=None):
-    if pl is None: pl = player()
-    return int(round(pl.location.z))
-
-def pos(pl=None):
-    if pl is None: pl = player()
-    return [x(pl), y(pl), z(pl)]
-
-################## MOVEMENT ##################
-
-def up(amount=1):
-    teleport(x(), y() + amount, z())
-
-def down(amount=1):
-    teleport(x(), y() - amount, z())
-
-def north(amount=1):
-    teleport(x() - amount, y(), z())
-
-def south(amount=1):
-    teleport(x() + amount, y(), z())
-
-def east(amount=1):
-    teleport(x(), y(), z() - amount)
-
-def west(amount=1):
-    teleport(x(), y(), z() + amount)
-
-reset_point = None
-
-def mark():
-    global reset_point
-    reset_point = pos()
-
-# go back to reset point
-def reset():
-    if reset_point:
-        teleport(reset_point)
-
-################## CREATION ##################
-
-def spawn(entitytype):
-    safe_entity = entity(entitytype)
-    if safe_entity is None:
-        raise Exception('Unknown entity type: ' + str(entitytype))
-        return
-    return _spawn(lookingat().location, safe_entity)
-
-@synchronous()
-def _spawn(location, entitytype):
-    return WORLD.spawnEntity(location, entitytype)
-
-def block(blocktype):
-    blocks(0, 0, 0, blocktype)
-
-def platform(blocktype=None, xradius=3, zradius=3):
-    blocks(blocktype, xradius, 0, zradius)
-
-def blocks(blocktype=None, xradius=3, yradius=3, zradius=3):
-    safe_blocktype = material(blocktype)
-    if safe_blocktype is None:
-        raise Exception('Unknown material type: ' + str(blocktype))
-        return
-    _blocks(safe_blocktype, lookingat().location, xradius, yradius, zradius)
-
-@synchronous()
-def _blocks(block_material, location, xradius, yradius, zradius):
-    for x in range(location.x - xradius, location.x + xradius + 1):
-        for y in range(location.y - yradius, location.y + yradius + 1):
-            for z in range(location.z - zradius, location.z + zradius + 1):
-                WORLD.getBlockAt(x, y, z).type = block_material
-
-@synchronous()
-def fill(pos, blocktype, srctype=None, maxdepth=20):
-    """
-    Flood fills an area of one block type to a different type.
-    """
-    if srctype is None:
-        srctype = WORLD.getBlockAt(pos[0], pos[1], pos[2]).type
-    _fill(pos, material(blocktype), material(srctype), 0, maxdepth)
-
-def _fill(pos, blocktype, srctype, depth, maxdepth):
-    if depth >= maxdepth:
-        return
-    px = pos[0] 
-    py = pos[1] 
-    pz = pos[2] 
-    block = WORLD.getBlockAt(px, py, pz)
-    if block.type != srctype:
-        return
-    block.type = blocktype
-    _fill([px+1, py, pz], blocktype, srctype, depth + 1, maxdepth)
-    _fill([px-1, py, pz], blocktype, srctype, depth + 1, maxdepth)
-    _fill([px, py, pz-1], blocktype, srctype, depth + 1, maxdepth)
-    _fill([px, py, pz+1], blocktype, srctype, depth + 1, maxdepth)
-    _fill([px, py-1, pz], blocktype, srctype, depth + 1, maxdepth)
 
 ################## SEARCHES ##################
 
 import re
-from java.lang import ClassLoader, Thread, Throwable
+from java.lang import Class, ClassLoader, Thread, Throwable
 from java.util import ArrayList, List
 
 def classes(s):
     """
     Searches for class names including the given string.
+
+    :param s: String fragment to match in class names.
     """
     # HACK: Obtain the list of all classes loaded by the current class loader.
     # Maybe this is not ideal, because it won't find classes that have not
@@ -207,8 +39,6 @@ def classes(s):
             pass
     return result
 
-from java.lang import Class
-
 def funcs(obj):
     """
     Prints the functions the given object possesses.
@@ -230,17 +60,6 @@ def funcs(obj):
             methods.add(m.getName() + "(?)")
     for m in methods:
         print("- " + m)
-
-@synchronous()
-def transform(blocktype=Material.AIR):
-    """
-    Converts what you are looking at into a block of the given material.
-    :param: blocktype The type of material your gaze shall inflict.
-
-    Examples:
-    - transform('water')
-    """
-    lookingat().type = material(blocktype)
 
 def _matching_fields(s, fields):
     results = []
@@ -274,6 +93,241 @@ def entities(s):
 
 def entity(e):
     return _single_match(e, EntityType)
+
+def pov(player=None, world=None):
+    return Perspective(player, world)
+
+################# CONTAINERS #################
+
+#class Region(object):
+#    def __init__(self):
+#        pass
+#    def pos():
+# Region is a collection of blocks
+# Region is iterable
+
+# Player is a 1-block Region aligned with a player's position.
+# Box is an WxHxD Region.
+# FloodFilled Region? With/without bounds?
+# Support for composite Regions? (and/or)
+# Other Regions are possible.
+
+class Perspective:
+    def __init__(self, player=None, world=None):
+        self.player = player
+        self._world = world
+        self.mark_points = {}
+        self.mark()
+
+    ################# ACCESSORS ##################
+
+    def world(self):
+        return self._world if self._world else self.player.world
+
+    def location(self, *args):
+        if len(args) == 0:
+            return self.player.getLocation()
+        if len(args) == 1:
+            return args[0].getLocation()
+        return Location(self.world(), *args)
+
+    def lookingat(self, distance=100):
+        return lookingat(self.player, distance)
+
+    ################# GAME MODES #################
+
+    def creative(self, player):
+        self.player.gameMode = GameMode.CREATIVE
+
+    def survival(self, player):
+        self.player.gameMode = GameMode.SURVIVAL
+
+    #################### TIME ####################
+
+    # NB: 24000 ticks per cycle.
+
+    @synchronous()
+    def time(self, time=None):
+        if time is None:
+            return self.world().getTime()
+        self.world().setTime(time)
+
+    def dawn(self):
+        self.time(22500)
+
+    def morning(self):
+        self.time(0)
+
+    def noon(self):
+        self.time(6000)
+
+    def dusk(self):
+        self.time(12500)
+
+    def night(self):
+        self.time(14000)
+
+    ################## WEATHER ###################
+
+    @synchronous()
+    def weather(self, raining=False, thunder=False):
+        self.world().setStorm(raining)
+        self.world().setThundering(thunder)
+
+    def sun(self):
+        self.weather(raining=False, thunder=False)
+
+    def rain(self):
+        self.weather(raining=True, thunder=False)
+
+    def storm(self):
+        self.weather(raining=True, thunder=True)
+
+    ################## VIOLENCE ##################
+
+    @synchronous()
+    def bolt(*args, **kwargs):
+        r = parseargswithpos(args, kwargs)
+        return self.world().strikeLightning(location(r['x'], r['y'], r['z']))
+
+    @synchronous()
+    def explosion(*args, **kwargs):
+        r = parseargswithpos(args, kwargs, ledger={'power':['power', 0, 8]})
+        return self.world().createExplosion(r['x'], r['y'], r['z'], r['power'], True)
+
+    def zap(self):
+        self.bolt(lookingat())
+
+    def boom(self, power=3):
+        self.explosion(lookingat(), power)
+
+    ################## POSITION ##################
+
+    @synchronous()
+    def teleport(self, *args, **kwargs):
+        r = parseargswithpos(args, kwargs, ledger={'whom':['whom', 0, None]})
+        if not r['whom']:
+            r['whom'] = self.player.getName()
+        someone = player(r['whom'])
+        someone.teleport(self.location(r['x'], r['y'], r['z']))
+
+    def x(self):
+        return int(round(self.player.location.x))
+
+    def y(self):
+        return int(round(self.player.location.y))
+
+    def z(self):
+        return int(round(self.player.location.z))
+
+    def pos(self):
+        return [self.x(), self.y(), self.z()]
+
+    ################## MOVEMENT ##################
+
+    def up(self, amount=1):
+        self.teleport(self.x(), self.y() + amount, self.z())
+
+    def down(self, amount=1):
+        self.teleport(self.x(), self.y() - amount, self.z())
+
+    def north(self, amount=1):
+        self.teleport(self.x() - amount, self.y(), self.z())
+
+    def south(self, amount=1):
+        self.teleport(self.x() + amount, self.y(), self.z())
+
+    def east(self, amount=1):
+        self.teleport(self.x(), self.y(), self.z() - amount)
+
+    def west(self, amount=1):
+        self.teleport(self.x(), self.y(), self.z() + amount)
+
+    def mark(self, label=None, pos=None):
+        """
+        Store the specified position with the given label.
+        """
+        self.mark_points[label] = pos if pos else self.pos()
+
+    def reset(self, label=None):
+        """
+        Go to the point marked with the given label.
+        """
+        self.teleport(self.mark_points[label])
+
+    ################## CREATION ##################
+
+    def spawn(self, entitytype):
+        safe_entity = entity(entitytype)
+        if safe_entity is None:
+            raise Exception('Unknown entity type: ' + str(entitytype))
+            return
+        return self._spawn(lookingat().location, safe_entity)
+
+    @synchronous()
+    def _spawn(self, location, entitytype):
+        return self.world().spawnEntity(location, entitytype)
+
+    def block(self, blocktype):
+        self.blocks(blocktype, 0, 0, 0)
+
+    def platform(self, blocktype=None, xradius=3, zradius=3):
+        self.blocks(blocktype, xradius, 0, zradius)
+
+    def blocks(self, blocktype=None, xradius=3, yradius=3, zradius=3):
+        if blocktype is None:
+            safe_blocktype = self.lookingat().type
+        else:
+            safe_blocktype = material(blocktype)
+        if safe_blocktype is None:
+            raise Exception('Unknown material type: ' + str(blocktype))
+            return
+        self._blocks(safe_blocktype, lookingat().location, xradius, yradius, zradius)
+
+    @synchronous()
+    def _blocks(self, block_material, location, xradius, yradius, zradius):
+        irange = lambda a, b: range(int(math.floor(a)), int(math.floor(b + 1)))
+        for x in irange(location.x - xradius, location.x + xradius):
+            for y in irange(location.y - yradius, location.y + yradius):
+                for z in irange(location.z - zradius, location.z + zradius):
+                    self.world().getBlockAt(x, y, z).type = block_material
+
+    @synchronous()
+    def fill(self, pos, blocktype, srctype=None, maxdepth=20):
+        """
+        Flood fills an area of one block type to a different type.
+        """
+        if srctype is None:
+            srctype = self.world().getBlockAt(pos[0], pos[1], pos[2]).type
+        _fill(pos, material(blocktype), material(srctype), 0, maxdepth)
+
+    def _fill(self, pos, blocktype, srctype, depth, maxdepth):
+        if depth >= maxdepth:
+            return
+        px = pos[0]
+        py = pos[1]
+        pz = pos[2]
+        block = self.world().getBlockAt(px, py, pz)
+        if block.type != srctype:
+            return
+        block.type = blocktype
+        self._fill([px+1, py, pz], blocktype, srctype, depth + 1, maxdepth)
+        self._fill([px-1, py, pz], blocktype, srctype, depth + 1, maxdepth)
+        self._fill([px, py, pz-1], blocktype, srctype, depth + 1, maxdepth)
+        self._fill([px, py, pz+1], blocktype, srctype, depth + 1, maxdepth)
+        self._fill([px, py-1, pz], blocktype, srctype, depth + 1, maxdepth)
+
+    @synchronous()
+    def transform(self, blocktype=Material.AIR):
+        """
+        Converts what you are looking at into a block of the given material.
+        :param: blocktype The type of material your gaze shall inflict.
+
+        Examples:
+        - transform('water')
+        """
+        self.lookingat().type = material(blocktype)
+
 
 # FIND OBJECTS
 #def findblock(pos, blocktype, maxradiusx=50, maxradiusy=50, maxradiusz=50):
