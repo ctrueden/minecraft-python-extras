56901cd825934476549d18b5d3aa2387299345d8
Curtis Rueden <ctrueden@wisc.edu>
2021-12-22 13:56:36 -0600

    golfast: add error checking
    With Java 17, the previous class loading approach no longer works.
    Rather than crash the entire startup, let's recover gracefully.

--- golfast.py
+++ golfast.py
@@ -3,13 +3,19 @@ from java.net import URL, URLClassLoader
 # TODO: Generalize path.
 url = URL('file:///home/curtis/minecraft/python/mcx/mcx.jar')
 cl = URLClassLoader([url])
-gol3d = cl.loadClass('GameOfLife3D')
-golFactory = gol3d.getConstructors()[0]
+try:
+    gol3d = cl.loadClass('GameOfLife3D')
+    golFactory = gol3d.getConstructors()[0]
+except:
+    print('Failed to load GameOfLife3D Java code.')
 
 def golfast(world, xMin, xMax, yMin, yMax, zMin, zMax,
             max_adjacent_dims=3, birth_min=6, birth_max=6,
             starvation_max=3, suffocation_min=8):
 
+    if not golFactory:
+        print('Sorry, the golfast function is not available.')
+        return
     return golFactory.newInstance(world, xMin, xMax, yMin, yMax, zMin, zMax,
                                   max_adjacent_dims, birth_min, birth_max,
                                   starvation_max, suffocation_min)
